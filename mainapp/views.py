2086a4429c94cc4b60a334b2f660a1b1329327d4
Anton Mindlin <anton.mindlin@gmail.com>
2021-12-29 00:39:19 +0200

    Lesson_3. Code fix

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,48 +1,41 @@
-from django.shortcuts import render
 from datetime import datetime
-from .models import ProductCategory, Product
+
 from django.conf import settings
+from django.shortcuts import render
+
+from .models import Product, ProductCategory
 
 
 def main(request):
-    title = 'главная'
-    products = Product.objects.all() 
-    content = {
-        'title': title,
-        'products': products,
-        'media_url': settings.MEDIA_URL
-    }
+    title = "главная"
+    products = Product.objects.all()
+    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
 
     return render(request, "mainapp/index.html", content)
 
 
 def products(request, pk=None):
-    title = 'продукты'
+    title = "продукты"
     links_menu = ProductCategory.objects.all()
     same_products = Product.objects.all()
     content = {
-        'title': title,
-        'links_menu': links_menu,
-        'same_products': same_products,
-        'media_url': settings.MEDIA_URL
+        "title": title,
+        "links_menu": links_menu,
+        "same_products": same_products,
+        "media_url": settings.MEDIA_URL,
     }
 
     if pk:
         print(pk)
-    
+
     return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
-    title = 'контакты'
+    title = "контакты"
     visit_date = datetime.now()
     contacts = [
-        {
-            "city": "Москва",
-            "phone": "+7-888-888-8888",
-            "email": "info@geekshop.ru",
-            "address": "В пределах МКАД"
-        },
+        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
         {
             "city": "Екатеринбург",
             "phone": "+7-777-777-7777",
@@ -54,12 +47,8 @@ def contact(request):
             "phone": "+7-999-999-9999",
             "email": "info_vladivostok@geekshop.ru",
             "address": "Близко к океану",
-        }
+        },
     ]
-    content = {
-        'title': title,
-        'visit_date': visit_date,
-        'contacts': contacts
-    }
+    content = {"title": title, "visit_date": visit_date, "contacts": contacts}
 
     return render(request, "mainapp/contact.html", content)
