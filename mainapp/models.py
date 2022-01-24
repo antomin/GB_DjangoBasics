d93914abb70191d2dabaa8af6faaa9d0077148ed
Anton Mindlin <anton.mindlin@gmail.com>
2022-01-24 15:30:09 +0100

    Lesson_8. Product CRUD

--- mainapp/models.py
+++ mainapp/models.py
@@ -18,7 +18,8 @@ class Product(models.Model):
     short_desc = models.CharField(verbose_name="краткое описание", max_length=60, blank=True)
     description = models.TextField(verbose_name="описание продукта", blank=True)
     price = models.DecimalField(verbose_name="цена продукта", max_digits=8, decimal_places=2, default=0)
-    quantity = models.PositiveIntegerField(verbose_name="количество на суладе", default=0)
+    quantity = models.PositiveIntegerField(verbose_name="количество на складе", default=0)
+    is_active = models.BooleanField(verbose_name="продукт активен", default=True)
 
     def __str__(self) -> str:
         return f"{self.name} {self.category.name}"
