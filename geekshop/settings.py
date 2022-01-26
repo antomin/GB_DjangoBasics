e3116b34ed48fa68e415e996e7568b5973a1db69
Anton Mindlin <37115351+antomin@users.noreply.github.com>
2022-01-26 13:51:33 +0100

    Lesson 8
    Lesson 8 ready.

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-b4k$k+o58ifyisctjat@q@^e7j5te+(t8#x8+%_m&$l)=p^uqm"
-)
+SECRET_KEY = "django-insecure-b4k$k+o58ifyisctjat@q@^e7j5te+(t8#x8+%_m&$l)=p^uqm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
