dc79a78954e27a5115076132ed02b93456104db3
Anton Mindlin <anton.mindlin@gmail.com>
2022-02-06 12:42:20 +0100

    lesson_11. Popup menu

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-b4k$k+o58ifyisctjat@q@^e7j5te+(t8#x8+%_m&$l)=p^uqm"
-)
+SECRET_KEY = "django-insecure-b4k$k+o58ifyisctjat@q@^e7j5te+(t8#x8+%_m&$l)=p^uqm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -172,9 +170,7 @@ AUTHENTICATION_BACKENDS = (
 
 import json
 
-with open(
-    os.path.join(BASE_DIR, "tmp", "secrets", "github.json"), "r"
-) as secrets:
+with open(os.path.join(BASE_DIR, "tmp", "secrets", "github.json"), "r") as secrets:
     github_auth = json.load(secrets)
 
 SOCIAL_AUTH_GITHUB_KEY = github_auth["client_id"]
