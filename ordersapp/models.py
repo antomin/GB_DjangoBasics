d2bd2ff79112923cff8a1eaec7276ce7378a9574
Anton Mindlin <anton.mindlin@gmail.com>
2022-02-08 14:55:55 +0100

    Errors

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -29,7 +29,7 @@ class Order(models.Model):
     created = models.DateTimeField(verbose_name="создан", auto_now_add=True)
     updated = models.DateTimeField(verbose_name="обновлён", auto_now=True)
     status = models.CharField(verbose_name="статус", max_length=3, choices=ORDER_STATUS_CHOICES, default=FORMING)
-    is_activ = models.BooleanField(verbose_name="активен", default=True)
+    is_active = models.BooleanField(verbose_name="активен", default=True)
 
     class Meta:
         ordering = ("-created",)
@@ -56,7 +56,7 @@ class Order(models.Model):
             item.product.quantity += item.quantity
             item.product.save()
 
-        self.is_activ = False
+        self.is_active = False
         self.save()
 
 
